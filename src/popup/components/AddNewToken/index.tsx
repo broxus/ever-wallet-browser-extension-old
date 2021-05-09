29d2e4df2d04fe2a8e1e3efe9dda53fd77b8f9de
Dasha Webb <ikariiii@gmail.com>
2021-05-09 20:57:28 +0300

    sliding panel unused renders fix

--- src/popup/components/AddNewToken/index.tsx
+++ src/popup/components/AddNewToken/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import cn from 'classnames'
 import { useForm } from 'react-hook-form'
 
--- src/popup/components/SlidingPanel/index.tsx
+++ src/popup/components/SlidingPanel/index.tsx
@@ -17,12 +17,14 @@ const SlidingPanel: React.FC<ISlidingPanel> = ({ isOpen, onClose, children }) =>
             {isOpen && <div className="sliding-panel__fade-in" />}
             <div className={cn('sliding-panel__wrapper', { _active: isOpen })}>
                 <div className="sliding-panel__wrapper__background" onClick={onClose} />
-                <div className={cn('sliding-panel__content')}>
-                    <div className="sliding-panel__content-header">
-                        <CloseIcon onClick={onClose} />
+                {isOpen && (
+                    <div className={cn('sliding-panel__content')}>
+                        <div className="sliding-panel__content-header">
+                            <CloseIcon onClick={onClose} />
+                        </div>
+                        {children}
                     </div>
-                    {children}
-                </div>
+                )}
             </div>
         </>
     )
