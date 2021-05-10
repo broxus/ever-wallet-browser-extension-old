8af2257668be3b6b2262727e5aca352cdaca887e
Dasha Webb <ikariiii@gmail.com>
2021-05-11 01:37:16 +0300

    new transactions layout, tooltip

--- src/popup/components/TransactionsListItem/index.tsx
+++ src/popup/components/TransactionsListItem/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react'
+import React, { useMemo } from 'react'
 import {
     extractTransactionValue,
     extractTransactionAddress,
@@ -8,11 +8,9 @@ import {
 import * as nt from '@nekoton'
 
 import TonLogoS from '@popup/img/ton-logo-s.svg'
-import Decimal from 'decimal.js'
+import ReactTooltip from 'react-tooltip'
 
 import './style.scss'
-import TransactionInfo from '@popup/components/TransactionInfo'
-import SlidingPanel from '@popup/components/SlidingPanel'
 
 type ITransactionsListItem = {
     transaction: nt.Transaction
@@ -28,6 +26,12 @@ const TransactionListItem: React.FC<ITransactionsListItem> = ({
     const value = useMemo(() => extractTransactionValue(transaction), [transaction])
     const txAddress = useMemo(() => extractTransactionAddress(transaction), [transaction])
 
+    // wip to hide tooltip on click outside
+
+    // for (const tooltip of document.querySelectorAll('.transactions-list-item__tooltip')) {
+    //     tooltip.addEventListener('click', (e) => e.stopPropagation())
+    // }
+
     return (
         <div
             className="transactions-list-item"
@@ -35,42 +39,51 @@ const TransactionListItem: React.FC<ITransactionsListItem> = ({
                 onViewTransaction(transaction)
             }}
         >
-            <div style={{ display: 'flex', width: '100%' }}>
-                <div style={{ marginRight: '16px', marginTop: '16px', minWidth: '36px' }}>
+            <div className="transactions-list-item__amount">
+                <div className="transactions-list-item__logo">
                     <TonLogoS />
                 </div>
-                <div className="transactions-list-item__description">
-                    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                        <span className="transactions-list-item__description__date">
-                            {new Date(transaction.createdAt * 1000).toLocaleTimeString()}
-                        </span>
-                    </div>
-                    <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'baseline' }}>
-                        <span className="transactions-list-item__description__address">
-                            {txAddress.address && convertAddress(txAddress.address)}
-                        </span>
-                        <span
-                            className={`transactions-list-item__description__${
-                                value.lessThan(0) ? 'expense' : 'income'
-                            }`}
-                        >
-                            {convertTons(value.toString())} TON
-                        </span>
+                <div>
+                    <div
+                        className={`transactions-list-item__description transactions-list-item__${
+                            value.lessThan(0) ? 'expense' : 'income'
+                        }`}
+                    >
+                        {convertTons(value.toString())} TON
                     </div>
                     <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                        <span className="transactions-list-item__description__fees">
+                        <span className="transactions-list-item__description transactions-list-item__fees">
                             Fees: {convertTons(transaction.totalFees)} TON
                         </span>
                     </div>
-                    {additionalInfo && (
-                        <span
-                            className="transactions-list-item__description__comment"
-                            style={{ color: '#000000', padding: '10px 0 0' }}
-                        >
-                            Staking reward.
-                        </span>
-                    )}
                 </div>
+            </div>
+
+            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                <span
+                    className="transactions-list-item__description transactions-list-item__address"
+                    data-tip={txAddress?.address}
+                >
+                    {txAddress.address && convertAddress(txAddress.address)}
+                </span>
+                <ReactTooltip
+                    className="transactions-list-item__tooltip"
+                    globalEventOff="click"
+                    type="dark"
+                    multiline
+                    clickable
+                    effect="solid"
+                    place="bottom"
+                />
+
+                <span className="transactions-list-item__description transactions-list-item__date">
+                    {new Date(transaction.createdAt * 1000).toLocaleString('default', {
+                        month: 'long',
+                        day: 'numeric',
+                        hour: 'numeric',
+                        minute: 'numeric',
+                    })}
+                </span>
             </div>
         </div>
     )
