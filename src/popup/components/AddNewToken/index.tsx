7e61952a1cb0e85090396228ecad520683f68560
Dasha Webb <ikariiii@gmail.com>
2021-05-10 15:49:51 +0300

    custom token form update

--- src/popup/components/AddNewToken/index.tsx
+++ src/popup/components/AddNewToken/index.tsx
@@ -11,6 +11,7 @@ import USDTLogo from '@popup/img/usdt-logo-s.svg'
 
 import './style.scss'
 import axios from 'axios'
+import Loader from '@popup/components/Loader'
 
 type PredefinedToken = {
     name: string
@@ -55,8 +56,8 @@ interface IToken {
     logoURI: string
     name: string
     symbol: string
-    enabled: boolean
-    onToggle: (enabled: boolean) => void
+    enabled?: boolean
+    onToggle?: (enabled: boolean) => void
 }
 
 export const Token: React.FC<IToken> = ({ logoURI, name, symbol, enabled, onToggle }) => {
@@ -77,7 +78,9 @@ export const Token: React.FC<IToken> = ({ logoURI, name, symbol, enabled, onTogg
                     <span className="assets-list-item__balance__dollars">{symbol}</span>
                 </div>
             </div>
-            <Tumbler checked={enabled} onChange={onToggle} id={name} />
+            {onToggle && enabled !== undefined && (
+                <Tumbler checked={enabled} onChange={onToggle} id={name} />
+            )}
         </div>
     )
 }
@@ -85,107 +88,81 @@ export const Token: React.FC<IToken> = ({ logoURI, name, symbol, enabled, onTogg
 type ISearchToken = {
     tokens: IAvailableToken[]
     onBack: () => void
+    onNext: (tokens: string[]) => void
 }
 
-enum SearchTokenStep {
+enum SelectTokenStep {
     SELECT,
     CONFIRM,
 }
-const SearchToken: React.FC<ISearchToken> = ({ tokens, onBack }) => {
+
+const SearchToken: React.FC<ISearchToken> = ({ tokens, onBack, onNext }) => {
     const [enabledTokens, setEnabledTokens] = useState<string[]>([])
-    const [step, setStep] = useState<SearchTokenStep>(SearchTokenStep.SELECT)
+
     const { register } = useForm()
 
-    const onSubmit = async () => {
-        console.log('submitted')
-    }
-
-    useEffect(() => {
-        console.log('enabledTokens', enabledTokens)
-    }, [enabledTokens])
-
     return (
-        <>
-            {step === SearchTokenStep.SELECT && (
-                <form>
-                    <Input
-                        label={'Enter token name...'}
-                        className="add-new-token__search-form"
-                        type="text"
-                        name="name"
-                        register={register()}
-                    />
-                    {/*{errors.name && <div className="check-seed__content-error">This field is required</div>}*/}
-                    <div style={{ overflowY: 'scroll', maxHeight: '320px', paddingRight: '8px' }}>
-                        {tokens.map(({ symbol, logoURI, name, address }) => {
-                            const makeOnToggle = (address: string) => (enabled: boolean) => {
-                                if (enabled) {
-                                    setEnabledTokens([...enabledTokens, address])
-                                } else {
-                                    setEnabledTokens(
-                                        enabledTokens.filter((item) => item !== address)
-                                    )
-                                }
-                            }
+        <form>
+            {/*<Input*/}
+            {/*    label={'Enter token name...'}*/}
+            {/*    className="add-new-token__search-form"*/}
+            {/*    type="text"*/}
+            {/*    name="name"*/}
+            {/*    register={register()}*/}
+            {/*/>*/}
+            {/*{errors.name && <div className="check-seed__content-error">This field is required</div>}*/}
+            <div style={{ overflowY: 'scroll', maxHeight: '320px', paddingRight: '8px' }}>
+                {tokens.map(({ symbol, logoURI, name, address }) => {
+                    const makeOnToggle = (address: string) => (enabled: boolean) => {
+                        if (enabled) {
+                            setEnabledTokens([...enabledTokens, address])
+                        } else {
+                            setEnabledTokens(enabledTokens.filter((item) => item !== address))
+                        }
+                    }
 
-                            return (
-                                <Token
-                                    key={symbol}
-                                    symbol={symbol}
-                                    logoURI={logoURI}
-                                    name={name}
-                                    enabled={enabledTokens.includes(address)}
-                                    onToggle={makeOnToggle(address)}
-                                />
-                            )
-                        })}
-                    </div>
-                    <div style={{ display: 'flex', paddingTop: '16px' }}>
-                        <div style={{ width: '50%', marginRight: '12px' }}>
-                            <Button text={'Back'} onClick={onBack} white />
-                        </div>
-                        <Button
-                            text={'Select assets'}
-                            disabled={!enabledTokens.length}
-                            onClick={() => setStep(SearchTokenStep.CONFIRM)}
+                    return (
+                        <Token
+                            key={symbol}
+                            symbol={symbol}
+                            logoURI={logoURI}
+                            name={name}
+                            enabled={enabledTokens.includes(address)}
+                            onToggle={makeOnToggle(address)}
                         />
-                    </div>
-                </form>
-            )}
-            {step === SearchTokenStep.CONFIRM && (
-                <>
-                    <h2>Do you want to add these assets?</h2>
-
-                    <div style={{ display: 'flex', paddingTop: '16px' }}>
-                        <div style={{ width: '50%', marginRight: '12px' }}>
-                            <Button
-                                text={'Back'}
-                                onClick={() => setStep(SearchTokenStep.SELECT)}
-                                white
-                            />
-                        </div>
-                        <Button text={'Yes, add selected assets'} onClick={() => onSubmit()} />
-                    </div>
-                </>
-            )}
-        </>
+                    )
+                })}
+            </div>
+            <div style={{ display: 'flex', paddingTop: '16px' }}>
+                <div style={{ width: '50%', marginRight: '12px' }}>
+                    <Button text={'Back'} onClick={onBack} white />
+                </div>
+                <Button
+                    text={'Select assets'}
+                    disabled={!enabledTokens.length}
+                    onClick={() => onNext(enabledTokens)}
+                />
+            </div>
+        </form>
     )
 }
 
 type ICustomToken = {
     onBack: () => void
+    onNext: () => void
 }
 
-const CustomToken: React.FC<ICustomToken> = ({}) => {
+const CustomToken: React.FC<ICustomToken> = ({ onBack, onNext }) => {
     const { register, handleSubmit, errors } = useForm<NewToken>()
 
     const onSubmit = async (data: NewToken) => {
         console.log('custom token submitted')
         console.log('contractAddress', data.rootTokenContract)
+        onNext()
     }
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <form>
             <Input
                 label={'Root token contract...'}
                 className="add-new-token__search-form"
@@ -198,6 +175,12 @@ const CustomToken: React.FC<ICustomToken> = ({}) => {
             {errors.rootTokenContract && (
                 <div className="check-seed__content-error">This field is required</div>
             )}
+            <div style={{ display: 'flex', paddingTop: '16px' }}>
+                <div style={{ width: '50%', marginRight: '12px' }}>
+                    <Button text={'Back'} onClick={onBack} white />
+                </div>
+                <Button text={'Proceed'} type="button" onClick={handleSubmit(onSubmit)} />
+            </div>
         </form>
     )
 }
@@ -214,6 +197,7 @@ enum Tab {
 const AddNewToken: React.FC<IAddNewToken> = ({ onBack }) => {
     const [activeTab, setActiveTab] = useState(Tab.PREDEFINED)
     const [availableTokens, setAvailableTokens] = useState<IAvailableToken[]>([])
+    const [step, setStep] = useState<SelectTokenStep>(SelectTokenStep.SELECT)
     const [selectedTokens, setSekectedTokens] = useState<string[]>()
 
     useEffect(() => {
@@ -222,34 +206,79 @@ const AddNewToken: React.FC<IAddNewToken> = ({ onBack }) => {
         })
     }, [])
 
+    const handleSelected = (tokens: string[]) => {
+        setSekectedTokens(tokens)
+        setStep(SelectTokenStep.CONFIRM)
+    }
+
+    const handleSubmit = () => {
+        console.log('selected tokens: ', selectedTokens)
+    }
+
     return (
         <>
-            <h2>Select new assets</h2>
-
-            <div className="add-new-token">
-                <div className="add-new-token__panel">
-                    <div
-                        className={cn('add-new-token__panel-tab', {
-                            _active: activeTab == Tab.PREDEFINED,
-                        })}
-                        onClick={() => setActiveTab(Tab.PREDEFINED)}
-                    >
-                        Search
+            {step === SelectTokenStep.SELECT && (
+                <>
+                    <h2>Select new assets</h2>
+                    <div className="add-new-token">
+                        <div className="add-new-token__panel">
+                            <div
+                                className={cn('add-new-token__panel-tab', {
+                                    _active: activeTab == Tab.PREDEFINED,
+                                })}
+                                onClick={() => setActiveTab(Tab.PREDEFINED)}
+                            >
+                                Search
+                            </div>
+                            <div
+                                className={cn('add-new-token__panel-tab', {
+                                    _active: activeTab == Tab.CUSTOM,
+                                })}
+                                onClick={() => setActiveTab(Tab.CUSTOM)}
+                            >
+                                Custom token
+                            </div>
+                        </div>
+                        {availableTokens.length === 0 && (
+                            <div
+                                style={{ width: '100%', display: 'flex', justifyContent: 'center' }}
+                            >
+                                <Loader />
+                            </div>
+                        )}
+                        {activeTab == Tab.PREDEFINED && (
+                            <SearchToken
+                                onBack={onBack}
+                                tokens={availableTokens}
+                                onNext={handleSelected}
+                            />
+                        )}
+                        {activeTab == Tab.CUSTOM && (
+                            <CustomToken onBack={onBack} onNext={() => {}} />
+                        )}
                     </div>
-                    <div
-                        className={cn('add-new-token__panel-tab', {
-                            _active: activeTab == Tab.CUSTOM,
-                        })}
-                        onClick={() => setActiveTab(Tab.CUSTOM)}
-                    >
-                        Custom token
+                </>
+            )}
+            {step === SelectTokenStep.CONFIRM && (
+                <div style={{ maxHeight: '500px', overflowY: 'auto' }}>
+                    <h2>Do you want to add these assets?</h2>
+                    {availableTokens
+                        .filter((item) => selectedTokens?.includes(item.address))
+                        .map(({ name, logoURI, symbol }) => (
+                            <Token key={symbol} name={name} logoURI={logoURI} symbol={symbol} />
+                        ))}
+                    <div style={{ display: 'flex', paddingTop: '16px' }}>
+                        <div style={{ width: '50%', marginRight: '12px' }}>
+                            <Button
+                                text={'Back'}
+                                onClick={() => setStep(SelectTokenStep.SELECT)}
+                                white
+                            />
+                        </div>
+                        <Button text={'Yes, add selected assets'} onClick={() => handleSubmit()} />
                     </div>
                 </div>
-                {activeTab == Tab.PREDEFINED && (
-                    <SearchToken onBack={onBack} tokens={availableTokens} />
-                )}
-                {activeTab == Tab.CUSTOM && <CustomToken onBack={onBack} />}
-            </div>
+            )}
         </>
     )
 }
