579e1b6d44d7526ef92b35111ee676c577dd132a
Dasha Webb <ikariiii@gmail.com>
2021-05-27 11:51:52 +0300

    select ledger key mocks replaces

--- src/popup/components/SelectLedgerAccount/index.tsx
+++ src/popup/components/SelectLedgerAccount/index.tsx
@@ -22,10 +22,10 @@ interface ILedgerAccount {
     checked: boolean
     setChecked: (arg0: boolean) => void
     address: string
-    balance: string
+    // balance: string
 }
 
-const LedgerAccount: React.FC<ILedgerAccount> = ({ address, balance, checked, setChecked }) => {
+const LedgerAccount: React.FC<ILedgerAccount> = ({ address, checked, setChecked }) => {
     return (
         <div className="select-ledger-account__account">
             <Checkbox checked={checked} setChecked={setChecked} />
@@ -34,66 +34,52 @@ const LedgerAccount: React.FC<ILedgerAccount> = ({ address, balance, checked, se
                 <div className="select-ledger-account__account-title">
                     {convertAddress(address)}
                 </div>
-                <div className="select-ledger-account__account-balance">
-                    {convertTons(balance)} TON
-                </div>
+                {/*<div className="select-ledger-account__account-balance">*/}
+                {/*    {convertTons(balance)} TON*/}
+                {/*</div>*/}
             </div>
         </div>
     )
 }
 
-const mockAccounts = [
-    {
-        address: '0:18b8fdf6bd451196ed70d649786ceb83cfd4a3f7be22f9f1cd09f8f9c80bc21b',
-        balance: '20000',
-    },
-    {
-        address: '0:2b68044eb2c8a3cd4d146ef7664ef6a49c099fc5c27c362cf5bd625f507e0410',
-        balance: '1000000',
-    },
-    {
-        address: '0:9f69bbae2f592031fee6b3811439259c0a85424d7c0f887958042aa2723daeb1',
-        balance: '2001200',
-    },
-    {
-        address: '0:18b8fdf6bd451196ed70d649786ceb83cfd4a3f7be22f9f1tt09f8f9c80bc21b',
-        balance: '18000',
-    },
-    {
-        address: '0:18b8fdf6bd451196ed70d649786tteb83cfd4a3f7be22f9f1cd09f8f9c80bc21b',
-        balance: '20000',
-    },
-    {
-        address: '0:18b8fdf6bd451196ed70d64978teb83cfd4a3f7bte22f9f1cd09f8f9c80bc21b',
-        balance: '18567',
-    },
-    {
-        address: '0:18b8fdf6bd451196ett0d649786ceb83cfd4a3f7be22f9f1cd09f8f9c80bc21b',
-        balance: '20000',
-    },
-]
+type LedgerAccountDetails = {
+    publicKey: string
+    index: number
+}
 
 const SelectLedgerAccount: React.FC<ISelectLedgerAccount> = ({ controllerRpc, onBack, onNext }) => {
-    const [currentPage, setCurrentPage] = useState(0)
-    // TODO update to list account number
-    const [selected, setSelected] = useState<string[]>([])
+    const [selected, setSelected] = useState<number[]>([])
+    const [accounts, setAccounts] = useState<LedgerAccountDetails[]>([])
 
-    const decrementIndex = () => {
-        setCurrentPage((currentPage + mockAccounts.length - 1) % mockAccounts.length)
+    const decrementIndex = async () => {
+        try {
+            const acc = await controllerRpc.getLedgerPreviousPage()
+            setAccounts(acc)
+        } catch (e) {
+            throw e
+        }
     }
 
-    const incrementIndex = () => {
-        setCurrentPage((currentPage + 1) % mockAccounts.length)
+    const incrementIndex = async () => {
+        try {
+            const acc = await controllerRpc.getLedgerNextPage()
+            setAccounts(acc)
+        } catch (e) {
+            throw e
+        }
+    }
+
+    const getFirstPage = async () => {
+        try {
+            const acc = await controllerRpc.getLedgerFirstPage()
+            setAccounts(acc)
+        } catch (e) {
+            throw e
+        }
     }
 
     useEffect(() => {
-        getFirstPage(controllerRpc)
-            .then((r) => {
-                console.log(r)
-            })
-            .catch((err) => {
-                console.log(err)
-            })
+        getFirstPage()
     }, [])
 
     return (
@@ -108,24 +94,21 @@ const SelectLedgerAccount: React.FC<ISelectLedgerAccount> = ({ controllerRpc, on
                 </div>
             </div>
             <div>
-                {mockAccounts
-                    .slice(currentPage * 5, (currentPage + 1) * 5)
-                    .map(({ address, balance }) => {
-                        const checked = selected.includes(address)
-                        return (
-                            <LedgerAccount
-                                key={address}
-                                address={address}
-                                balance={balance}
-                                checked={checked}
-                                setChecked={() => {
-                                    checked
-                                        ? setSelected(selected.filter((el) => el !== address))
-                                        : setSelected([...selected, address])
-                                }}
-                            />
-                        )
-                    })}
+                {accounts.map(({ publicKey, index }) => {
+                    const checked = selected.includes(index)
+                    return (
+                        <LedgerAccount
+                            key={publicKey}
+                            address={publicKey}
+                            checked={checked}
+                            setChecked={() => {
+                                checked
+                                    ? setSelected(selected.filter((el) => el !== index))
+                                    : setSelected([...selected, index])
+                            }}
+                        />
+                    )
+                })}
             </div>
 
             <div className="select-ledger-account__buttons">
